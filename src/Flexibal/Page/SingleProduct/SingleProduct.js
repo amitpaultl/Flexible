e9de257e1b8c8404a71a6c9416e0311c71570478
amitpaultl <amitpaultl@gmail.com>
2023-02-03 10:48:59 +0600

    single Product add page

--- src/Flexibal/Page/SingleProduct/SingleProduct.js
+++ src/Flexibal/Page/SingleProduct/SingleProduct.js
@@ -19,6 +19,9 @@ import SectionTitle from "../../Componte/SectionTitle/SectionTitle";
 import ReletatProduct from "../../Componte/ReletatProduct/ReletatProduct";
 
 
+
+
+
 const SingleProduct = () => {
     const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
@@ -28,8 +31,8 @@ const SingleProduct = () => {
                 <div className="row">
 
                     <div className="col-lg-5">
-                        {/* <div className="product-slider">
-                       
+                        <div className="product-slider">
+
                                 <Swiper
                                     style={{
                                         "--swiper-navigation-color": "#fff",
@@ -66,57 +69,12 @@ const SingleProduct = () => {
                                     <SwiperSlide>
                                         <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
                                     </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-                                    </SwiperSlide>
+                                    
+                                    
                                 </Swiper>
-                                <Swiper
-                                    onSwiper={setThumbsSwiper}
-                                    loop={true}
-                                    spaceBetween={10}
-                                    slidesPerView={4}
-                                    freeMode={true}
-                                    watchSlidesProgress={true}
-                                    modules={[FreeMode, Navigation, Thumbs]}
-                                    className="mySwiper"
-                                >
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-                                    </SwiperSlide>
-                                    <SwiperSlide>
-                                        <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-                                    </SwiperSlide>
-                                </Swiper>
-                      
+                                
 
-                        </div> */}
+                        </div>
                     </div>
                     <div className="col-md-7">
                         <div className="product-details-content quickview-content">
@@ -373,6 +331,7 @@ const SingleProduct = () => {
                 </div>
             </div>
         </div>
+       
     );
 };
 
